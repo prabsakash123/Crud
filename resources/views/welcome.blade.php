93b722ea42ceed6edb19a156ff8399c52188efd5
Programming Akash <mauryaakash505@gmail.com>
2021-05-27 18:17:57 +0530

    update view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Welcome To Report Details Employee</title>
+        <title>Welcome To Report Details Student</title>
 
         <!-- Fonts -->
 		<!-- CSS only -->
